1338157bbfb99fbf9c4670893344d741f35f2278
Michael Clifford <cliffom@gmail.com>
2016-01-27 15:12:26 -0800

    :penguin: Initial tests

--- spec/tokenex_spec.rb
+++ spec/tokenex_spec.rb
@@ -1,11 +1,40 @@
 require 'spec_helper'
 
 describe Tokenex do
+  def tokenex
+    api_base_url = ENV["TOKENEX_API_BASE_URL"]
+    token_ex_id = ENV["TOKENEX_ID"]
+    api_key = ENV["TOKENEX_API_KEY"]
+    Tokenex::Environment.new(api_base_url, token_ex_id, api_key)
+  end
+  
+  def input_ccnum
+    4242424242424242
+  end
+  
+  def token
+    tokenex.token_from_ccnum(input_ccnum)
+  end
+  
+  def ccnum
+    tokenex.ccnum_from_token(token['Token'])
+  end
+
   it 'has a version number' do
     expect(Tokenex::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'tokenizes a credit card' do
+    expect(token['Token']).not_to be nil
+    expect(token['Success']).to be true
+  end
+  
+  it 'detokenizes a credit card' do
+    expect(ccnum['Value']).not_to be nil
+    expect(token['Success']).to be true
+  end
+  
+  it 'returns valid data' do
+    expect(input_ccnum === ccnum['Value'].to_i).to be true
   end
 end

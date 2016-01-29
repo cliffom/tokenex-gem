06b73f4edc963af0cbab7ffa5dd6a9c760a4add0
Michael Clifford <cliffom@gmail.com>
2016-01-29 11:57:20 -0800

    :penguin: v0.4.0
    * API Complete with TokenEx Token Services
    (http://docs.tokenex.com/#tokenex-api-token-services)

--- lib/tokenex/environment.rb
+++ lib/tokenex/environment.rb
@@ -4,69 +4,82 @@ require "uri"
 
 module Tokenex
     class Environment
-    
+
         def initialize(api_base_url, tokenex_id, api_key, options={})
             @api_base_url = api_base_url
             @tokenex_id = tokenex_id
             @api_key = api_key
         end
-        
-        def token_from_ccnum(ccNum)
+
+        def token_from_ccnum(ccnum, token_scheme = 3)
             catch (:tokenex_cannot_tokenize_data) do
-                return tokenize(ccNum, 3)
+                return tokenize(ccnum, token_scheme)
             end
             throw :tokenex_invalid_ccnum
         end
-        
-        def tokenize(data_to_tokenize, token_scheme = 4)
+
+        def tokenize(data, token_scheme = 4)
             action = "Tokenize"
-            data = {
-                "Data" => data_to_tokenize,
+            request_parameters = {
+                "Data" => data,
                 "TokenScheme" => token_scheme
             }
-            
-            response = send_request(action, data)
+
+            response = send_request(action, request_parameters)
             throw :tokenex_cannot_tokenize_data unless is_valid_response(response)
-            
+
             return response['Token']
         end
-        
+
+        def tokenize_from_encrypted_value(encrypted_data, token_scheme)
+            action = "TokenizeFromEncryptedValue"
+            request_parameters = {
+                "EcryptedData" => encrypted_data,
+                "TokenScheme" => token_scheme
+            }
+
+            response = send_request(action, request_parameters)
+            throw :tokenex_cannot_tokenize_from_encrypted_value unless is_valid_response(response)
+
+            return response['Token']
+        end
+
         def detokenize(token)
             action = "Detokenize"
-            data = {
+            request_parameters = {
                 "Token" => token
             }
-            
-            response = send_request(action, data)
-            throw :tokenex_invalid_token unless is_valid_response(response) 
-            
+
+            response = send_request(action, request_parameters)
+            throw :tokenex_invalid_token unless is_valid_response(response)
+
             return response['Value']
         end
-        
+
         def validate_token(token)
             action = "ValidateToken"
-            data = {
+            request_parameters = {
                 "Token" => token
             }
-            
-            response = send_request(action, data)
-            throw :tokenex_invalid_token unless is_valid_response(response) 
-            
+
+            response = send_request(action, request_parameters)
+            throw :tokenex_invalid_token unless is_valid_response(response)
+
             return response['Valid']
         end
-        
+
         def delete_token(token)
             action = "DeleteToken"
-            data = {
+            request_parameters = {
                 "Token" => token
             }
-            
-            response = send_request(action, data)
-            throw :tokenex_invalid_token unless is_valid_response(response) 
-            
+
+            response = send_request(action, request_parameters)
+            throw :tokenex_invalid_token unless is_valid_response(response)
+
             return response['Success']
         end
-        
+
         private
         def headers
             {
@@ -74,32 +87,32 @@ module Tokenex
                 'Accept' => 'application/json'
             }
         end
-        
+
         def build_request_array(data)
             request_array = {
                 "APIKey" => @api_key,
                 "TokenExID" => @tokenex_id
             }.merge(data)
-             
+
             return request_array
         end
 
         def send_request(action, data)
             request_body = build_request_array(data).to_json
-            
+
             uri = URI.parse("#{@api_base_url}#{action}")
             http = Net::HTTP.new(uri.host, uri.port)
             http.use_ssl = true
-            
+
             request = Net::HTTP::Post.new(uri, initheader = headers)
             request.body = request_body
             response = http.request(request)
             return JSON.parse(response.body)
         end
-        
+
         def is_valid_response(response)
             return !response['Success'].nil? && response['Success'] === true
         end
-        
+
     end
 end
--- spec/tokenex_spec.rb
+++ spec/tokenex_spec.rb
@@ -7,23 +7,23 @@ describe Tokenex do
     api_key = ENV["TOKENEX_API_KEY"]
     Tokenex::Environment.new(api_base_url, token_ex_id, api_key)
   end
-  
+
   def valid_ccnum
     4242424242424242
   end
-  
+
   def invalid_ccnum
     1234567812345678
   end
-  
+
   def invalid_token
     'abcdefg'
   end
-  
+
   def token
     tokenex.token_from_ccnum(valid_ccnum)
   end
-  
+
   def ccnum
     tokenex.detokenize(token)
   end
@@ -35,48 +35,59 @@ describe Tokenex do
   it 'tokenizes a credit card' do
     expect(token).not_to be nil
   end
-  
+
   it 'detokenizes a credit card' do
     expect(ccnum).not_to be nil
   end
-  
+
   it 'returns valid data' do
     expect(valid_ccnum === ccnum.to_i).to be true
   end
-  
+
   it 'handles bad credit cards' do
     expect { tokenex.token_from_ccnum(invalid_ccnum) }.to throw_symbol(:tokenex_invalid_ccnum)
   end
-  
+
   it 'handles bad tokens' do
     expect { tokenex.detokenize(invalid_token) }.to throw_symbol(:tokenex_invalid_token)
   end
-  
+
   it 'can verify a token exists' do
     expect(tokenex.validate_token(token)).to be true
   end
-  
+
   it 'can verify a token does not exist' do
     expect(tokenex.validate_token(invalid_token)).to be false
   end
-  
+
   it 'can delete a token' do
-    token_to_delete = token 
+    token_to_delete = token
     expect(tokenex.validate_token(token_to_delete)).to be true
     expect(tokenex.delete_token(token)).to be true
     expect(tokenex.validate_token(token_to_delete)).to be false
   end
-  
+
   it 'will not delete a token that does not exist' do
     expect(tokenex.validate_token(invalid_token)).to be false
     expect { tokenex.delete_token(invalid_token) }.to throw_symbol(:tokenex_invalid_token)
   end
-  
+
   it 'can tokenize arbitrary data' do
     data = "This is my string with 3 numbers less than 10"
     token = tokenex.tokenize(data, 4)
-    data_detokenized = tokenex.detokenize(token)
     expect(tokenex.validate_token(token)).to be true
-    expect(data === data_detokenized).to be true
+    data_detokenized = tokenex.detokenize(token)
+    expect(data_detokenized).to eq(data)
+
   end
+
+  it 'can tokenize from an encrypted value' do
+      encrypted_value = "FWOd2HUAI+AYfaC3PKAz4dugByBdd+fEAzFfg/G41UuM8yFK23qoq8oD6CURF5WZpXPXySYbN8XvRM6Pd8dfQCTcSQdGiSBfD+Csv39XbOS/laAIekYsPav/eAnY+tNAV7sGUvtqOnDDr0H9W6Q8Z6nqL0rdezCIDY7DuNcOUZxPsv4EV2djG75c9oXI7rPUa/CtIxp1GOCkPYhkV4pv6sxoYOBAJ2KrMDgzlZS9UWQE5x346Jc8ixEOA0bWItTcXUW8/hITYAlM1mTKqRX/Er7Mag2uBrpNM/t5HNtw/zVNgwc8S4ltvm7ow3IG98K2cDpEi16ly2QuMiL5Iq8ghw=="
+      token = tokenex.tokenize_from_encrypted_value(encrypted_value, 1);
+      expect(tokenex.validate_token(token)).to be true
+      data_from_token = tokenex.detokenize(token)
+      expect(data_from_token).to eq(valid_ccnum.to_s)
+      expect(tokenex.delete_token(token)).to be true
+  end
+
 end
